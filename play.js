e05d0ef02ccb859648e92f8e47e2507081e8dd33
garaddel <garaddel@gmail.com>
2023-06-07 14:42:21 -0400

    Added ctrlC termination option + handleUserInput function

--- play.js
+++ play.js
@@ -3,3 +3,20 @@ const { connect } = require("./client.js");
 
 console.log("Connecting ...");
 connect();
+
+// setup interface to handle user input from stdin
+
+const setupInput = function () {
+  stdin.on("data", handleUserInput);
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+};
+
+const handleUserInput = function () {
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
